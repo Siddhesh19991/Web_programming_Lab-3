7357b06355cf22a387968506b3f55c80f0935818
NAZLIBILGIC <nazli.blgc@gmail.com>
2024-02-19 13:27:36 +0100

    db

--- schema.sql
+++ schema.sql
@@ -1,13 +1,24 @@
-create table user(firstname varchar(100), familyname varchar(100)  
-      ,gender varchar(100),city varchar(100), country varchar(100),
-       email varchar(100),
-       password varchar(100),
-       messages BLOB,
-       primary key(email));
+create table user(
+      firstname varchar(100),
+      familyname varchar(100),
+      gender varchar(100),
+      city varchar(100),
+      country varchar(100),
+      email varchar(100),
+      password varchar(100),
+      primary key(email)
+);
  
 
  create table token_data(token varchar(100),
        email varchar(100),
        primary key(email));
  
- 
+
+create table messages(
+      sender varchar(100),
+      receiver varchar(100),
+      message_data text,
+      foreign key(sender) references user(email),
+      foreign key(receiver) references user(email)
+)
